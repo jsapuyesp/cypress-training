c2a9dee22455687e9ec32bce71cdbc4cc5d812e8
Juan Jose Sapuyes <jsapuyesp@gmail.com>
2022-04-18 11:05:44 -0500

    Punto7

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -3,19 +3,36 @@ describe("Buy a t-shirt", () => {
     cy.visit("http://automationpractice.com/");
     cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
     cy.get(
-      "#center_column a.button.ajax_add_to_cart_button.btn.btn-default",
+      "#center_column a.button.ajax_add_to_cart_button.btn.btn-default"
     ).click();
-    cy.get("[style*=\"display: block;\"] .button-container > a").click();
+    cy.get('[style*="display: block;"] .button-container > a').click();
     cy.get(".cart_navigation span").click();
 
     cy.get("#email").type("aperdomobo@gmail.com");
     cy.get("#passwd").type("WorkshopProtractor");
 
     // Debes completar la prueba ... implementa los pasos 8 al 13, del proceso de compra
+    cy.get("#SubmitLogin").click();
+
+    cy.get(
+      "#center_column button.button.btn.btn-default.button-medium"
+    ).click();
+
+    cy.get("#form > div > p > div > span > input").click();
+
+    cy.get(
+      "#center_column button.button.btn.btn-default.button-medium"
+    ).click();
+
+    cy.get("#center_column a.bankwire").click();
+
+    cy.get(
+      "#center_column button.button.btn.btn-default.button-medium"
+    ).click();
 
     cy.get("#center_column > div > p > strong").should(
       "have.text",
-      "Your order on My Store is complete.",
+      "Your order on My Store is complete."
     );
   });
 });
